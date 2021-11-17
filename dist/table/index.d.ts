379699bc074624edd339f437f555cb5677bbdf78
babbage <601728820@qq.com>
2021-11-17 11:11:09 +0800

    feat: 版本升级至0.0.2

--- dist/table/index.d.ts
+++ dist/table/index.d.ts
@@ -4,7 +4,7 @@ export interface ITableItem {
     uly: string;
     khan: string;
 }
-export declare const BOUND_FLAG: string;
+export declare const BOUND_FLAG = "'";
 export declare const HEMZE = "\u0626\u200D";
 declare const table: ITableItem[];
 export { table as default };
